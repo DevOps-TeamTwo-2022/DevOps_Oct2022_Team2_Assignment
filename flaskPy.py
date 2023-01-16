235e30b46aeb5d6a8cd03321b05b4abb7237c12c
DandaIT04 <s10208266@connect.np.edu.sg>
2023-01-16 17:10:12 +0800

    base match student, minimal layout, navbar attempted

--- flaskPy.py
+++ flaskPy.py
@@ -1,17 +1,27 @@
 from flask import Flask,render_template,redirect,url_for
 
 # for vscode, type python flaskPy.py in terminal to run at 5221 port
+# 1) pytest tests/flaskTest.py to run test script
+# python -m pytest tests/flaskTest.py if 1) doesn't work
+# pytest -s prints console
 
 app = Flask(__name__)
 
+# Redirect http://localhost:5221/ to http://localhost:5221/main
 @app.route("/")
 def redirectToMain():
-    return redirect(url_for("hello_world"))
+    return redirect(url_for("mainFile"))
 
-@app.route("/main")
-def hello_world():
+# {{helloVar}} in index gets its value from here
+@app.route("/Main")
+def mainFile():
     helloVar = "Hello world"
     return render_template("index.html",helloVar=helloVar)
+
+@app.route("/Match_Student")
+def matchFile():
+    helloVar_Match = "Hello world Match"
+    return render_template("Match_Student.html",helloVar_Match=helloVar_Match)
 
 
 if __name__ == '__main__':
--- tests/flaskTest.py
+++ tests/flaskTest.py
@@ -1,21 +1,27 @@
 import pytest 
 from flaskPy import *
-from flaskPy import app # Flask instance of the API
+from flaskPy import app
 
+#tests if server error occurred or redirection not done e.g redirect from / to /main automatically
 def flaskPy_testOne_redirection():
   
   response = app.test_client().get('/')
   
-  #assert "302 FOUND" in response, "Redirection works!"
+  match_Open = app.test_client().get('/Match_Student')
   
-  passPassTest = []
-  PassFailTest = []
+  tempList = [str(response),str(match_Open)]
+  
+  print("Redirect to main",tempList[0],"Open Match_Student.html",tempList[1])
+  
+  errors = []
 
-  #if "302 FOUND" in response:
-  #    testing.append("Redirection works!")
-  if not "302 FOUND" in response:
-      testing.append("Warning! Redirection Failed!")
+  if "500 internal" in tempList[0]:
+    errors.append("Warning! 500 internal server error!")   
+  if not "302 FOUND" in tempList[0]:
+    errors.append("Warning! Redirection not found!")
+  if "404 NOT FOUND" in tempList[1]:
+    errors.append("Warning! 404 Not Found for Match_Student.html!")     
 
-  assert not testing, "Tests occured:\n{}".format("\n".join(testing))  
+  assert not errors, "errors occured:\n{}".format("\n".join(errors))  
     
 flaskPy_testOne_redirection()
