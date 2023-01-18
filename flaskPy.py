76d821850f93ce5eed9a03522c31b39f505dcd41
DandaIT04 <s10208266@connect.np.edu.sg>
2023-01-18 16:56:40 +0800

    recursion to get table rows

--- flaskPy.py
+++ flaskPy.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,redirect,url_for
 import pyodbc
+import re
 
 ## for vscode, type python flaskPy.py in terminal to run at 5221 port
 # 1) pytest tests/flaskTest.py to run test script
@@ -12,46 +13,48 @@ app = Flask(__name__)
 
 
 # Use this for local sql database
-"""
+
 cnxn = pyodbc.connect(
     'DRIVER={ODBC Driver 17 for SQL Server}; \
         SERVER=(localdb)\MSSQLLocalDB; \
             DATABASE=DevOps_TeamTwo_2022; \
             Trusted_Connection=yes;',autocommit = True)
+
 """
-
-
-# use this for github action collection database
+# IMPORTANT! use this for github action container database
 cnxn = pyodbc.connect(
     'DRIVER={ODBC Driver 17 for SQL Server}; \
         SERVER=(localdb)\MSSQLLocalDB; \
             DATABASE=tempdb; \
             UID=sa; \
             PWD=dbatools.I0;',autocommit = True)
-
-
+"""
 
 # Create a cursor from the connection
 cursor = cnxn.cursor()
 
+# If table does not exist in database, store in toCreate
+# Then create the table later on
 table_List = ['Internship_Student_Data','Internship_Company_Data','Internship_Information_Data']
 
 toCreate = []
 
 for i in table_List:
     if cursor.tables(table=i).fetchone(): 
-        print("yes table: ",i)
+        print("Tables that exists: ",i)
     else:
-        print("Doesn't exist: ",i)
+        print("Tables that don't exist: ",i)
         toCreate.append(i)      
 
 s = "SELECT StudentID, Name,Preference, \
     Status FROM Internship_Student_Data; \
         SELECT ID, Company_Name,Job_Role \
-            FROM Internship_Company_Data;"
+            FROM Internship_Company_Data; \
+            SELECT StudentID, ID \
+            FROM Internship_Information_Data;"
 
 # FOR LOCAL USE! DO NOT DELETE! START
-"""           
+        
 for i in toCreate:
     if i == "Internship_Student_Data":
         s = "CREATE TABLE Internship_Student_Data(StudentID varchar(10) NOT NULL PRIMARY KEY CLUSTERED,Name varchar(255) NOT NULL,Preference varchar(255) NOT NULL,Status varchar(11) NOT NULL); \
@@ -64,10 +67,14 @@ for i in toCreate:
             SELECT StudentID, Name,Preference, \
             Status FROM Internship_Student_Data; \
             SELECT ID, Company_Name,Job_Role \
-            FROM Internship_Company_Data;"  
-"""
+            FROM Internship_Company_Data; \
+            SELECT StudentID, ID \
+            FROM Internship_Information_Data;"  
+
 # FOR LOCAL USE! DO NOT DELETE! END
 
+# For github action container use DO NOT DELETE
+"""
 for i in toCreate:
     if i == "Internship_Student_Data":
         s = "CREATE TABLE Internship_Student_Data(StudentID varchar(10) NOT NULL PRIMARY KEY CLUSTERED,Name varchar(255) NOT NULL,Preference varchar(255) NOT NULL,Status varchar(11) NOT NULL); \
@@ -77,14 +84,40 @@ for i in toCreate:
             Status FROM Internship_Student_Data; \
             SELECT ID, Company_Name,Job_Role \
             FROM Internship_Company_Data;" 
-           
+"""  
+# For github action container use DO NOT DELETE  
+    
 s = filter(None, s.split(';'))
+
+studentList = []
+companyList = []
+informationList = []
+
+def addToList(row):
+    tempStr = str(row)
+    newTempStr = re.sub('[^.,a-zA-Z0-9 \n\.]', '', tempStr)
+    
+    tempList = newTempStr.split(',')
+       
+    return tempList
 
 for i in s:
     cursor.execute(i.strip() + ';')
-    if "SELECT" in i:
+    if "Internship_Student_Data" in i:
         for row in cursor:
-            print(row)     
+            studentList.append(addToList(row))
+            
+    elif "Internship_Company_Data" in i:
+        for row in cursor:
+            companyList.append(addToList(row))
+            
+    elif "Internship_Information_Data" in i:
+        for row in cursor:
+            informationList.append(addToList(row))
+
+print("student: ",studentList)
+print("company: ",companyList)
+print("information: ",informationList)
 
 # Redirect http://localhost:5221/ to http://localhost:5221/main
 @app.route("/")
