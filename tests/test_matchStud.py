4eb19a876edadd8ff65d798128e0587aa3c420ed
DandaIT04 <s10208266@connect.np.edu.sg>
2023-01-22 22:37:06 +0800

    Session variable to store temp html values and match students checks/testing done

---
+++ tests/test_matchStud.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+def test_scenario1(): #route to match student via navbar
+    driver = webdriver.Chrome \
+        (service=ChromeService \
+            (executable_path=ChromeDriverManager().install()))
+
+    driver.get("http://localhost:5221")
+    
+    time.sleep(1)
+
+    title = driver.title
+    assert title == "Home - My Webpage"
+
+    driver.implicitly_wait(0.5)
+
+    time.sleep(1)
+    
+    original_window = driver.current_window_handle  
+    
+    driver.find_element(By.XPATH, \
+        "//a[@href='/Match_Student']").click()
+    
+    time.sleep(1)
+
+    for window_handle in driver.window_handles:
+        if window_handle != original_window:
+            driver.switch_to.window(window_handle)
+            break    
+
+    time.sleep(1)
+
+    title = driver.title
+    assert title == "Match Students - My Webpage"
+    
+    time.sleep(1)
+
+    driver.quit()
